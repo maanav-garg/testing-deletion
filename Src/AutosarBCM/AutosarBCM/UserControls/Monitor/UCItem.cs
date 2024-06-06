7b71139ebd26e303ffae5b2317efee8a1c874618
ozankan <ozankan@VEARGL30406>
2024-06-06 14:32:24 +0300

    fix: ucItem update

--- Src/AutosarBCM/AutosarBCM/UserControls/Monitor/UCItem.cs
+++ Src/AutosarBCM/AutosarBCM/UserControls/Monitor/UCItem.cs
@@ -348,10 +348,9 @@ namespace AutosarBCM.UserControls.Monitor
         {
             lbResponse.Items.Clear();
             lbResponse.Items.AddRange(ControlInfo.GetPayloads(ServiceInfo.ReadDataByIdentifier, null).ToArray());
-            lblTransmitted.Text = "0";
-            lblReceived.Text = "0";
-            MessageTransmitted = 0;
-            MessageReceived = 0;
+            oldValue = null;
+            lblTransmitted.Text = lblReceived.Text = "0";
+            MessageTransmitted = MessageReceived = 0;            
         }
 
         private void UCItem_Load(object sender, EventArgs e)
