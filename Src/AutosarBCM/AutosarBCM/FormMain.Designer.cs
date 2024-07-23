2ee559cd56d8be6b6a32abb4e5f0b1ecb7dcf9f6
handeac <Hande.Acar@vestel.com.tr>
2024-07-23 16:47:19 +0300

    fix: button location edited

--- Src/AutosarBCM/AutosarBCM/FormMain.Designer.cs
+++ Src/AutosarBCM/AutosarBCM/FormMain.Designer.cs
@@ -199,10 +199,10 @@ namespace AutosarBCM
             this.tslReceived,
             this.tslTransmitted,
             this.tslDiff});
-            this.toolStrip1.Location = new System.Drawing.Point(0, 28);
+            this.toolStrip1.Location = new System.Drawing.Point(0, 30);
             this.toolStrip1.Name = "toolStrip1";
             this.toolStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.System;
-            this.toolStrip1.Size = new System.Drawing.Size(1889, 27);
+            this.toolStrip1.Size = new System.Drawing.Size(1889, 31);
             this.toolStrip1.TabIndex = 2;
             this.toolStrip1.Text = "toolStrip1";
             // 
@@ -317,7 +317,7 @@ namespace AutosarBCM
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
             this.menuStrip1.Padding = new System.Windows.Forms.Padding(5, 2, 0, 2);
-            this.menuStrip1.Size = new System.Drawing.Size(1889, 28);
+            this.menuStrip1.Size = new System.Drawing.Size(1889, 30);
             this.menuStrip1.TabIndex = 1;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -415,11 +415,11 @@ namespace AutosarBCM
             this.tabControl1.Controls.Add(this.tabPage2);
             this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tabControl1.ImageList = this.imageList1;
-            this.tabControl1.Location = new System.Drawing.Point(0, 55);
+            this.tabControl1.Location = new System.Drawing.Point(0, 61);
             this.tabControl1.Margin = new System.Windows.Forms.Padding(4);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(1889, 922);
+            this.tabControl1.Size = new System.Drawing.Size(1889, 916);
             this.tabControl1.TabIndex = 15;
             // 
             // tabPage3
@@ -430,7 +430,7 @@ namespace AutosarBCM
             this.tabPage3.Location = new System.Drawing.Point(4, 25);
             this.tabPage3.Margin = new System.Windows.Forms.Padding(4);
             this.tabPage3.Name = "tabPage3";
-            this.tabPage3.Size = new System.Drawing.Size(1881, 893);
+            this.tabPage3.Size = new System.Drawing.Size(1881, 887);
             this.tabPage3.TabIndex = 2;
             this.tabPage3.Text = "Monitor";
             this.tabPage3.UseVisualStyleBackColor = true;
@@ -439,12 +439,12 @@ namespace AutosarBCM
             // 
             this.dockMonitor.Dock = System.Windows.Forms.DockStyle.Fill;
             this.dockMonitor.DockBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(238)))), ((int)(((byte)(238)))), ((int)(((byte)(242)))));
-            this.dockMonitor.Location = new System.Drawing.Point(0, 28);
+            this.dockMonitor.Location = new System.Drawing.Point(0, 31);
             this.dockMonitor.Margin = new System.Windows.Forms.Padding(4, 0, 4, 4);
             this.dockMonitor.Name = "dockMonitor";
             this.dockMonitor.Padding = new System.Windows.Forms.Padding(6);
             this.dockMonitor.ShowAutoHideContentOnHover = false;
-            this.dockMonitor.Size = new System.Drawing.Size(1881, 865);
+            this.dockMonitor.Size = new System.Drawing.Size(1881, 856);
             this.dockMonitor.TabIndex = 2;
             this.dockMonitor.Theme = this.vS2015LightTheme1;
             this.dockMonitor.ActiveDocumentChanged += new System.EventHandler(this.dockMonitor_ActiveDocumentChanged);
@@ -473,7 +473,7 @@ namespace AutosarBCM
             this.tsbToggle});
             this.toolStrip3.Location = new System.Drawing.Point(0, 0);
             this.toolStrip3.Name = "toolStrip3";
-            this.toolStrip3.Size = new System.Drawing.Size(1881, 28);
+            this.toolStrip3.Size = new System.Drawing.Size(1881, 31);
             this.toolStrip3.TabIndex = 1;
             this.toolStrip3.Text = "toolStrip3";
             // 
@@ -499,9 +499,7 @@ namespace AutosarBCM
             // nullToolStripMenuItem
             // 
             this.nullToolStripMenuItem.Name = "nullToolStripMenuItem";
-
-            this.nullToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-
+            this.nullToolStripMenuItem.Size = new System.Drawing.Size(119, 26);
             this.nullToolStripMenuItem.Text = "N/A";
             // 
             // tsbECUReset
--- Src/AutosarBCM/AutosarBCM/UserControls/Monitor/UCItem.Designer.cs
+++ Src/AutosarBCM/AutosarBCM/UserControls/Monitor/UCItem.Designer.cs
@@ -44,10 +44,11 @@
             // 
             this.lblName.AutoEllipsis = true;
             this.lblName.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.lblName.Location = new System.Drawing.Point(3, 10);
-            this.lblName.MaximumSize = new System.Drawing.Size(170, 20);
+            this.lblName.Location = new System.Drawing.Point(4, 12);
+            this.lblName.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblName.MaximumSize = new System.Drawing.Size(227, 25);
             this.lblName.Name = "lblName";
-            this.lblName.Size = new System.Drawing.Size(170, 20);
+            this.lblName.Size = new System.Drawing.Size(227, 25);
             this.lblName.TabIndex = 0;
             this.lblName.Text = "label1";
             this.lblName.Click += new System.EventHandler(this.lblName_Click);
@@ -55,9 +56,10 @@
             // lblStatus
             // 
             this.lblStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.lblStatus.Location = new System.Drawing.Point(200, 7);
+            this.lblStatus.Location = new System.Drawing.Point(267, 9);
+            this.lblStatus.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblStatus.Name = "lblStatus";
-            this.lblStatus.Size = new System.Drawing.Size(10, 10);
+            this.lblStatus.Size = new System.Drawing.Size(13, 12);
             this.lblStatus.TabIndex = 1;
             this.lblStatus.Text = "-";
             this.lblStatus.Visible = false;
@@ -67,9 +69,10 @@
             this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label1.Location = new System.Drawing.Point(268, 6);
+            this.label1.Location = new System.Drawing.Point(357, 7);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(17, 20);
+            this.label1.Size = new System.Drawing.Size(22, 25);
             this.label1.TabIndex = 5;
             this.label1.Text = "↑";
             // 
@@ -77,9 +80,10 @@
             // 
             this.lblTransmitted.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.lblTransmitted.AutoSize = true;
-            this.lblTransmitted.Location = new System.Drawing.Point(281, 13);
+            this.lblTransmitted.Location = new System.Drawing.Point(375, 16);
+            this.lblTransmitted.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblTransmitted.Name = "lblTransmitted";
-            this.lblTransmitted.Size = new System.Drawing.Size(13, 13);
+            this.lblTransmitted.Size = new System.Drawing.Size(14, 16);
             this.lblTransmitted.TabIndex = 6;
             this.lblTransmitted.Text = "0";
             // 
@@ -87,9 +91,10 @@
             // 
             this.lblReceived.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.lblReceived.AutoSize = true;
-            this.lblReceived.Location = new System.Drawing.Point(326, 13);
+            this.lblReceived.Location = new System.Drawing.Point(435, 16);
+            this.lblReceived.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblReceived.Name = "lblReceived";
-            this.lblReceived.Size = new System.Drawing.Size(13, 13);
+            this.lblReceived.Size = new System.Drawing.Size(14, 16);
             this.lblReceived.TabIndex = 7;
             this.lblReceived.Text = "0";
             // 
@@ -98,9 +103,10 @@
             this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label2.Location = new System.Drawing.Point(311, 6);
+            this.label2.Location = new System.Drawing.Point(415, 7);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(17, 20);
+            this.label2.Size = new System.Drawing.Size(22, 25);
             this.label2.TabIndex = 8;
             this.label2.Text = "↓";
             // 
@@ -108,17 +114,19 @@
             // 
             this.lblDiff.AutoSize = true;
             this.lblDiff.ForeColor = System.Drawing.Color.White;
-            this.lblDiff.Location = new System.Drawing.Point(216, 35);
+            this.lblDiff.Location = new System.Drawing.Point(288, 43);
+            this.lblDiff.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblDiff.Name = "lblDiff";
-            this.lblDiff.Size = new System.Drawing.Size(0, 13);
+            this.lblDiff.Size = new System.Drawing.Size(0, 16);
             this.lblDiff.TabIndex = 9;
             // 
             // btnRead
             // 
             this.btnRead.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnRead.Location = new System.Drawing.Point(216, 9);
+            this.btnRead.Location = new System.Drawing.Point(288, 11);
+            this.btnRead.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnRead.Name = "btnRead";
-            this.btnRead.Size = new System.Drawing.Size(46, 23);
+            this.btnRead.Size = new System.Drawing.Size(61, 28);
             this.btnRead.TabIndex = 10;
             this.btnRead.Text = "Read";
             this.btnRead.UseVisualStyleBackColor = true;
@@ -131,25 +139,28 @@
             this.lbResponse.Font = new System.Drawing.Font("Consolas", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lbResponse.FormattingEnabled = true;
             this.lbResponse.ItemHeight = 15;
-            this.lbResponse.Location = new System.Drawing.Point(6, 39);
+            this.lbResponse.Location = new System.Drawing.Point(8, 48);
+            this.lbResponse.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.lbResponse.Name = "lbResponse";
-            this.lbResponse.Size = new System.Drawing.Size(343, 79);
+            this.lbResponse.Size = new System.Drawing.Size(456, 94);
             this.lbResponse.TabIndex = 0;
             this.lbResponse.DrawItem += new System.Windows.Forms.DrawItemEventHandler(this.lbResponse_DrawItem);
             // 
             // btnUCClear
             // 
+            this.btnUCClear.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.btnUCClear.Image = global::AutosarBCM.Properties.Resources.delete_icon_12_795858790;
-            this.btnUCClear.Location = new System.Drawing.Point(181, 9);
+            this.btnUCClear.Location = new System.Drawing.Point(241, 11);
+            this.btnUCClear.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnUCClear.Name = "btnUCClear";
-            this.btnUCClear.Size = new System.Drawing.Size(32, 23);
+            this.btnUCClear.Size = new System.Drawing.Size(43, 28);
             this.btnUCClear.TabIndex = 11;
             this.btnUCClear.UseVisualStyleBackColor = true;
             this.btnUCClear.Click += new System.EventHandler(this.btnUCClear_Click);
             // 
             // UCItem
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.btnUCClear);
             this.Controls.Add(this.lbResponse);
@@ -161,8 +172,9 @@
             this.Controls.Add(this.lblTransmitted);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.lblName);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "UCItem";
-            this.Size = new System.Drawing.Size(355, 125);
+            this.Size = new System.Drawing.Size(473, 154);
             this.Load += new System.EventHandler(this.UCItem_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
