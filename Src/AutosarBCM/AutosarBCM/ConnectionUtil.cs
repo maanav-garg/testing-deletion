d26e80766292408f40adb4e6e4055e1e3e75cb39
sergenesendemirV <sergene@vestel.com.tr>
2024-08-13 11:08:21 +0300

    AppendTrace added instead showErrorMessageBox

--- Src/AutosarBCM/AutosarBCM/ConnectionUtil.cs
+++ Src/AutosarBCM/AutosarBCM/ConnectionUtil.cs
@@ -19,6 +19,7 @@ using Connection.Protocol.Uds;
 using AutosarBCM.Config;
 using System.Collections.Specialized;
 using System.Runtime.InteropServices;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TaskbarClock;
 
 namespace AutosarBCM
 {
@@ -142,7 +143,8 @@ namespace AutosarBCM
 
         private void TransportProtocol_ReceiveError(object sender, Connection.Protocol.TransportErrorEventArs e)
         {
-            Helper.ShowErrorMessageBox(e.Message);
+            AppendTrace(e.Message, DateTime.Now, Color.Red);
+            //Helper.ShowErrorMessageBox(e.Message);
             //if (e. == CanHardware_ErrorStatus.Disconnect)
             //    Disconnect();
         }
@@ -453,7 +455,9 @@ namespace AutosarBCM
         /// <param name="e">The event arguments containing error information.</param>
         private void SerialHardware_ErrorAccured(object sender, SerialPortErrorEventArgs e)
         {
-            Helper.ShowErrorMessageBox(e.Message);
+            var time = new DateTime((long)e.Timestamp);
+            AppendTrace(e.Message, time, Color.Red);
+            //Helper.ShowErrorMessageBox(e.Message);
             if (e.ErrorType == SerialHardware_ErrorType.Disposed)
                 Disconnect();
         }
@@ -556,7 +560,9 @@ namespace AutosarBCM
         /// <param name="e">The event arguments containing error information.</param>
         private void Hardware_CanError(object sender, CanErrorEventArgs e)
         {
-            Helper.ShowErrorMessageBox(e.ErrorMessage);
+            var time = new DateTime((long)e.Timestamp);
+            AppendTrace(e.ErrorMessage, time, Color.Red);
+            //Helper.ShowErrorMessageBox(e.ErrorMessage);
             if (e.Status == CanHardware_ErrorStatus.Disconnect)
                 Disconnect();
         }
